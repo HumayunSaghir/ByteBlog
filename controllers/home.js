66b137f23f51693ba1096769b812b460d35d7df2
HumayunSaghir <hr3296726@gmail.com>
2026-05-15 20:35:18 +0500

    added profilepage

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,6 @@
 const blogModel = require("../models/blogs")
+const userModel = require("../models/users")
+const {createToken} = require("../services/auth")
 
 async function handleShowHomepage(req, res){
     if(req.user === undefined){
@@ -14,6 +16,42 @@ async function handleShowHomepage(req, res){
     })
 }
 
+async function handleShowProfilePage(req, res){
+    const yourBlogs = await blogModel.find({createdBy : req.user._id})
+    return res.status(200).render("profilePage", {
+        user : req.user,
+        name : req.user.name,
+        email : req.user.email,
+        profileImageUrl : req.user.profileImageUrl,
+        allBlogs : yourBlogs
+    })
+}
+
+async function handleChangeProfileImage(req, res){
+    return res.status(200).render("changeProfileImage", {
+        user : req.user
+    })
+}
+
+async function handleChangeImage(req, res){
+    const reqUser = await userModel.findById(req.user._id)
+
+    reqUser.profileImageUrl = `/profileImages/${req.file.filename}`
+    await reqUser.save()
+
+    req.user.profileImageUrl = reqUser.profileImageUrl
+
+    // we need to clear and update the token
+    res.clearCookie("token")
+    const token = createToken(reqUser)
+    res.cookie("token", token)
+
+    return res.status(200).redirect('/user/profile')
+}
+
 module.exports = {
     handleShowHomepage,
+    handleShowProfilePage,
+    handleChangeProfileImage,
+    handleChangeImage,
 }
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,24 @@
+const multer = require("multer")
+const path = require("path")
 const {Router} = require("express")
-const {handleShowHomepage} = require("../controllers/home")
+const {handleShowHomepage, handleShowProfilePage, handleChangeProfileImage, handleChangeImage} = require("../controllers/home")
 
 const router = Router()
 
+// multer configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/profileImages/`))
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`)
+  }
+})
+const upload = multer({ storage: storage })
+
 router.get("/", handleShowHomepage)
+router.get("/user/profile", handleShowProfilePage)
+router.get("/user/changeProfileImage", handleChangeProfileImage)
+router.post("/user/changeProfileImage",upload.single("profileImage"), handleChangeImage)
 
 module.exports = router
